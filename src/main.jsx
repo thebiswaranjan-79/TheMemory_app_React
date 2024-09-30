d68e0ac300ff1d0bcee7876115606c1aff0ea67b
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-09-30 17:31:41 +0530

    Setup Basic Redux

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
- import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import pasteReducers from "./redux/pasteSlice";
 
- export const store = configureStore({
-    reducer: {},
- })
+export const store = configureStore({
+  reducer: {
+    paste: pasteReducers,
+  },
+});
